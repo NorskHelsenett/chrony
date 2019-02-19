6c87a237c7e5c81911be68159149bf616dc21931
Thomas Juberg <thomas.juberg@gmail.com>
2019-02-19 22:32:08 +0100

    Update to newer syntax

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 describe 'chrony' do
   context 'with default values for all parameters' do
-    it { should contain_class('chrony') }
+    it { is_expected.to contain_class('chrony') }
   end
 end
